752584e198bebcd71dc53d9dd39be50e538be3d5
Ryan Tanaka <tanaka@isi.edu>
2022-02-11 09:57:40 -0800

    added bank example

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
-def increment(x):
-    return x+1
+class Bank:
+
+    def __init__(self):
+        self._accounts = dict()
+
+    def deposit_money(self, account: int, amount: float) -> None:
+        if amount <= 0:
+            raise ValueError("Deposit amount should be greater than 0!")
+
+        try:
+            self._accounts[account] += amount
+        except KeyError as e:
+            raise ValueError("Account does not exist!")
+
+    def get_balance(self, account) -> float:
+        try:
+            return self._accounts[account]
+        except KeyError as e:
+            raise ValueError("Account does not exist!")
 
