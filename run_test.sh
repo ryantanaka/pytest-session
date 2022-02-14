6cbdc1c35717946102ce7f1416c123b6ea8efe6e
Ryan Tanaka <tanaka@isi.edu>
2022-02-14 08:35:28 -0800

    updated run_test script so that we can install pytest

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash 
 
+python3 -c "import pytest" &> /dev/null
+if [ "$(echo $?)" -eq 1 ]
+then
+    pip3 install pytest pytest-cov
+fi
+
 export PYTHONPATH=$(pwd)/src
-$(pwd)/venv/bin/pytest --cov-report term --cov-report html --cov=src
+pytest --cov-report term --cov-report html --cov=src
